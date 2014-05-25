24433a348411131d9f387cbf14ac988a6ab14416
Matías Susena Tizón <roexone@gmail.com>
2014-05-25 21:38:58 +0200

    Bajada de posición boton "volver" en formulario Mati
    Bajada de posición boton "volver" en formulario Mati

--- ProyectoFinalSmithJones/ProyectoFinalSmithJones/VentanaBoton2.Designer.cs
+++ ProyectoFinalSmithJones/ProyectoFinalSmithJones/VentanaBoton2.Designer.cs
@@ -98,7 +98,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(281, 335);
+            this.button1.Location = new System.Drawing.Point(281, 344);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 6;
